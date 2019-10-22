881c735c3d67e492b3fd2c874e45a16e7814206e
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2019-10-22 13:09:44 +0530

    fixed app name

--- config/settings_staging.py
+++ config/settings_staging.py
@@ -30,19 +30,19 @@ DATABASES = {
 SITE_DOMAIN = 'https://learning1.shine.com/chatbot/'
 
 ###### STORAGE SETTINGS #############
-DEFAULT_FILE_STORAGE = 'core.library.gcloud.custom_cloud_storage.GCPMediaStorage'
+DEFAULT_FILE_STORAGE = 'apps.core.library.gcloud.custom_cloud_storage.GCPMediaStorage'
 GS_BUCKET_NAME = 'learning-media-staging-189607'
 
-PRIVATE_MEDIA_FILE_STORAGE = 'core.library.gcloud.custom_cloud_storage.GCPPrivateMediaStorage'
+PRIVATE_MEDIA_FILE_STORAGE = 'apps.core.library.gcloud.custom_cloud_storage.GCPPrivateMediaStorage'
 GCP_PRIVATE_MEDIA_BUCKET = 'learning--misc-staging-189607'
 GCP_RESUME_BUILDER_BUCKET = 'learning--misc-staging-189607'
 CRM_PRIVATE_MEDIA_BUCKET = 'learningcrm-misc-staging-189607'
 
-COMPRESS_STORAGE = STATICFILES_STORAGE = 'core.library.gcloud.custom_cloud_storage.GCPStaticStorage'
+COMPRESS_STORAGE = STATICFILES_STORAGE = 'apps.core.library.gcloud.custom_cloud_storage.GCPStaticStorage'
 GS_PROJECT_ID = 'shine-staging-189607'
 GCP_STATIC_BUCKET = 'learning-static-staging-189607'
 
-INVOICE_FILE_STORAGE = 'core.library.gcloud.custom_cloud_storage.GCPInvoiceStorage'
+INVOICE_FILE_STORAGE = 'apps.core.library.gcloud.custom_cloud_storage.GCPInvoiceStorage'
 GCP_INVOICE_BUCKET = 'learning-invoices-staging-189607'
 
 GCP_MEDIA_LOCATION = "c/m/"

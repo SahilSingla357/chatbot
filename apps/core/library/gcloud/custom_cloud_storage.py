d2cfed953cafb13fbd0ae4d7e967797853da91f2
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2019-10-22 14:21:14 +0530

    changes

--- apps/core/library/gcloud/custom_cloud_storage.py
+++ apps/core/library/gcloud/custom_cloud_storage.py
@@ -29,8 +29,8 @@ class GCPStaticStorage(GoogleCloudStorageMixin):
 
     def __init__(self, **settings):
         super(GCPStaticStorage, self).__init__(**settings)
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
+        # self.local_storage = get_storage_class(
+        #     "compressor.storage.CompressorFileStorage")()
 
     def url(self, name):
         return settings.STATIC_URL + name
